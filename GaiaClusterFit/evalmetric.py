00e675a5bff6b6877bd9ab915dbad6cba86d762c
levi223 <levi2234@hotmail.com>
2022-11-04 13:02:37 +0100

    replaced files

--- GaiaClusterFit/evalmetric.py
+++ GaiaClusterFit/evalmetric.py
@@ -1,7 +1,7 @@
 import sklearn as sk
 import numpy as np
 
-def homogeneityscore(dataselection, regiondata):
+def homogeneityscore(dataselection, regiondata,data=None):
     """Cross-match-scores 2 sets of clustered data on a homogeneity score
     Args:
         dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
@@ -17,9 +17,9 @@ def homogeneityscore(dataselection, regiondata):
     true_common_elements = regiondata[common_elements_region].group_by("source_id")
         
     score = sk.metrics.homogeneity_score(true_common_elements["population"], predicted_common_elements["population"])
-    return 
+    return score
 
-def completenessscore(dataselection, regiondata):
+def completenessscore(dataselection, regiondata,data=None):
     """Cross-match-scores 2 sets of clustered data on a homogeneity score
     Args:
         dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
@@ -35,10 +35,10 @@ def completenessscore(dataselection, regiondata):
     true_common_elements = regiondata[common_elements_region].group_by("source_id")
         
     score = sk.metrics.completeness_score(true_common_elements["population"], predicted_common_elements["population"])
-    return 
+    return score
 
 
-def randscore(dataselection, regiondata):
+def randscore(dataselection, regiondata,data=None):
     """Cross-match-scores 2 sets of clustered data on a homogeneity score
     Args:
         dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
@@ -54,9 +54,9 @@ def randscore(dataselection, regiondata):
     true_common_elements = regiondata[common_elements_region].group_by("source_id")
         
     score = sk.metrics.rand_score(true_common_elements["population"], predicted_common_elements["population"])
-    return 
+    return score
 
-def calinskiharabaszscore(dataselection, regiondata):
+def calinskiharabaszscore(dataselection, regiondata,data=None):
     """Cross-match-scores 2 sets of clustered data on a homogeneity score
     Args:
         dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
@@ -72,9 +72,9 @@ def calinskiharabaszscore(dataselection, regiondata):
     true_common_elements = regiondata[common_elements_region].group_by("source_id")
         
     score = sk.metrics.calinski_harabasz_score(true_common_elements["population"], predicted_common_elements["population"])
-    return 
+    return score
 
-def mutualinfoscore(dataselection, regiondata):
+def mutualinfoscore(dataselection, regiondata,data=None):
     """Cross-match-scores 2 sets of clustered data on a homogeneity score
     Args:
         dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
@@ -90,9 +90,9 @@ def mutualinfoscore(dataselection, regiondata):
     true_common_elements = regiondata[common_elements_region].group_by("source_id")
         
     score = sk.metrics.mutual_info(true_common_elements["population"], predicted_common_elements["population"])
-    return 
+    return score
 
-def daviesbouldinscore(dataselection, regiondata):
+def daviesbouldinscore(dataselection, regiondata,data=None):
     """Cross-match-scores 2 sets of clustered data on a homogeneity score
     Args:
         dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
@@ -108,16 +108,16 @@ def daviesbouldinscore(dataselection, regiondata):
     true_common_elements = regiondata[common_elements_region].group_by("source_id")
         
     score = sk.metrics.davies_bouldin_score(true_common_elements["population"], predicted_common_elements["population"])
-    return
+    return score
 
-def vmeasurescore(dataselection, regiondata):
-    """Cross-match-scores 2 sets of clustered data on a homogeneity score
+def vmeasurescore(dataselection, regiondata,data=None):
+    """Cross-match-scores 2 sets of clustered data on a v-measure score
     Args:
         dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
         regiondata (astropy.Table): Astropy Table that includes all imported luster data .
 
     Returns:
-        Float: The return value. True for success, False otherwise.
+        Float: Score between 0 and 1
     """
 
     common_elements_data = np.isin(dataselection["source_id"],regiondata["source_id"])
@@ -126,4 +126,41 @@ def vmeasurescore(dataselection, regiondata):
     true_common_elements = regiondata[common_elements_region].group_by("source_id")
         
     score = sk.metrics.v_measure_score(true_common_elements["population"], predicted_common_elements["population"])
-    return
+    return score
+
+def vmeasurescore(dataselection, regiondata,data=None):
+    """Cross-match-scores 2 sets of clustered data on a vmeasure score
+    Args:
+        dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
+        regiondata (astropy.Table): Astropy Table that includes all imported luster data .
+
+    Returns:
+        Float: The return value.
+    """
+
+    common_elements_data = np.isin(dataselection["source_id"],regiondata["source_id"])
+    common_elements_region = np.isin(regiondata["source_id"],dataselection["source_id"])
+    predicted_common_elements = dataselection[common_elements_data].group_by("source_id")
+    true_common_elements = regiondata[common_elements_region].group_by("source_id")
+        
+    score = sk.metrics.v_measure_score(true_common_elements["population"], predicted_common_elements["population"])
+    return score
+
+def silhouettescore(dataselection, regiondata,data=None):
+    """Cross-match-scores 2 sets of clustered data on a homogeneity score
+    Args:
+        dataselection (astropy.Table): Astropy Table that includes all imported Gaia data of the Queried region.
+        regiondata (astropy.Table): Astropy Table that includes all imported luster data .
+
+    Returns:
+        Float: The return value. True for success, False otherwise.
+    """
+    try:
+        score = sk.metrics.silhouette_score(np.array(data).T,dataselection["population"])
+        print(score)
+        return score
+
+    except:
+        print("Could not compute score")
+        return float("nan")
+
